e7d562185039ed1f02a17ebf7b2c7bb36ca4b20c
SGJUNG <playmymind@daum.net>
2019-07-27 23:31:26 +0900

    Slack OAuth 인증정보 교환로직  : Step 3 - Exchanging a verification code for an access token (50%)  : 코드 백업

--- stock-data-api/src/main/java/com/share/data/api/auth/AuthController.java
+++ stock-data-api/src/main/java/com/share/data/api/auth/AuthController.java
@@ -2,11 +2,11 @@ package com.share.data.api.auth;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Controller
 public class AuthController {
@@ -18,13 +18,32 @@ public class AuthController {
     }
 
     @CrossOrigin(origins = {"http://slack.com", "https://slack.com"})
-    @GetMapping(value = "/auth/slack/redirect", produces = "application/json")
-    public String redirect(@RequestParam("code") String code , @RequestParam("state") String state, Model model){
+    @GetMapping(value = "/auth/slack/redirect", produces = "application/x-www-form-urlencoded")
+    public String redirect(@RequestParam("code") String code , @RequestParam("state") String state,
+                           HttpServletRequest request, HttpServletResponse response, Model model){
         System.out.println("code    :: " + code);
         System.out.println("state   :: " + state);
 
+//        response.setHeader("Accept-Encoding", request.getHeader("Accept-Encoding"));
+////        response.setHeader("Content-Encoding", "gzip");
+//        response.setHeader("Accept", request.getHeader("Accept"));
+//        response.setHeader("access-control-allow-origin:", request.getHeader("*"));
+//        response.setHeader("content-type", "application/json; charset=utf-8");
+
         model.addAttribute("code", code);
         model.addAttribute("state", state);
+        return "test-slack-signin";
+    }
+
+    @CrossOrigin(origins = {"http://slack.com", "https://slack.com"})
+    @PostMapping("/auth/slack/redirect")
+    public String redirect(@RequestBody Map<String, Object> params, Model model){
+        String code = String.valueOf(params.get("code"));
+        String state = String.valueOf(params.get("state"));
+
+        model.addAttribute("code", code);
+        model.addAttribute("state", state);
+        System.out.println("POST REDIRECT...");
         return "test-slack-signin";
     }
 
