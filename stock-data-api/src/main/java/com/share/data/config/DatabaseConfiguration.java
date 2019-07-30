e06f98428a50fd688f4711af9304e3f57afd85d4
SGJUNG <playmymind@daum.net>
2019-07-31 08:47:14 +0900

    Mybatis 설정 추가 (진행중)  - SqlSessionFactory, SqlSessionTemplate, HikariConfig 수정

--- stock-data-api/src/main/java/com/share/data/config/DatabaseConfiguration.java
+++ stock-data-api/src/main/java/com/share/data/config/DatabaseConfiguration.java
@@ -2,7 +2,13 @@ package com.share.data.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,6 +18,11 @@ import javax.sql.DataSource;
 @Configuration
 @PropertySource("classpath:/application.properties")
 public class DatabaseConfiguration {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public HikariConfig hikariConfig(){
@@ -22,5 +33,18 @@ public class DatabaseConfiguration {
     public DataSource dataSource(){
         DataSource dataSource = new HikariDataSource();
         return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResource("classpath:/mapper/**/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory){
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
