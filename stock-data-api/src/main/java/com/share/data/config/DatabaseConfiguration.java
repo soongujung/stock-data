4fc67999749b5a9fbef2c673031e344c6aed4236
SGJUNG <playmymind@daum.net>
2019-08-01 09:11:53 +0900

    Mybatis 설정 추가 (진행중)  - 참고 자료 주석으로 추가

--- stock-data-api/src/main/java/com/share/data/config/DatabaseConfiguration.java
+++ stock-data-api/src/main/java/com/share/data/config/DatabaseConfiguration.java
@@ -8,6 +8,7 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,15 +24,26 @@ public class DatabaseConfiguration {
     private ApplicationContext applicationContext;
 
 
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+//    public HikariConfig hikariConfig(){
+//        return new HikariConfig();
+//    }
+
+    /**
+     * https://jojoldu.tistory.com/296
+     * https://stackoverflow.com/questions/23287462/spring-boot-fails-to-load-datasource-using-postgresql-driver
+     * http://blog.naver.com/PostView.nhn?blogId=admass&logNo=220979632501&beginTime=0&jumpingVid=&from=search&redirect=Log&widgetTypeCall=true
+     * @return
+     */
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
-    public HikariConfig hikariConfig(){
-        return new HikariConfig();
-    }
-
-    @Bean
     public DataSource dataSource(){
-        DataSource dataSource = new HikariDataSource(hikariConfig());
+        DataSource dataSource =
+                DataSourceBuilder.create()
+                        .type(HikariDataSource.class)
+                        .build();
+
         return dataSource;
     }
 
