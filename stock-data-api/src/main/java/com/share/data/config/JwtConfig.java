95cf041e02fb577db98f71356ff82aaedf942043
SGJUNG <playmymind@daum.net>
2019-08-08 09:00:20 +0900

    JWT/OAuth  - pom.xml, 설정파일 작성 (진행중)

---
+++ stock-data-api/src/main/java/com/share/data/config/JwtConfig.java
@@ -0,0 +1,14 @@
+package com.share.data.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+
+@Configuration
+public class JwtConfig {
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
+
+
+}
--- stock-data-api/src/main/java/com/share/data/config/WebSecurityConfig.java
+++ stock-data-api/src/main/java/com/share/data/config/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.share.data.config;
 
+import com.share.data.api.auth.UserAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -25,6 +30,9 @@ import java.util.Arrays;
  */
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserAuthService userAuthService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -59,6 +67,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userAuthService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 }
