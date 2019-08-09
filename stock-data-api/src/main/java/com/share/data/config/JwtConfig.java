9b92d8c3f80549a5fd2629c004ec42f136fa3286
SGJUNG <playmymind@daum.net>
2019-08-09 16:40:56 +0900

    JWT, Security 연동 설정 추가  : KeyStore 관련 File Not Found Error 수정  : ClassPathResource 대신 FileSystemResource 를 사용하도록 변경  ( 참고자료 : https://docs.spring.io/spring/docs/2.5.x/javadoc-api/org/springframework/core/io/Resource.html?is-external=true )

--- stock-data-api/src/main/java/com/share/data/config/JwtConfig.java
+++ stock-data-api/src/main/java/com/share/data/config/JwtConfig.java
@@ -3,21 +3,16 @@ package com.share.data.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
-import javax.sql.DataSource;
 import java.security.KeyPair;
 
 @Configuration
@@ -31,43 +26,48 @@ public class JwtConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private ClientDetailsService clientDetailsService;
 
-    @Bean
-    public TokenStore tokenStore(){
-        return new JwtTokenStore(accessTokenConverter());
-    }
+//    @Bean
+//    public TokenStore tokenStore(){
+//        return new JwtTokenStore(accessTokenConverter());
+//    }
 
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
 
-        // 내용 찾아보자...
+        String path = this.getClass().getClassLoader().getResource("keystore/test.jks").getPath();
+        System.out.println("path :: " + path);
+
         KeyPair keyPair =
                 new KeyStoreKeyFactory(
-                    new ClassPathResource("server.jks"),
-                    "passtwo".toCharArray()
-
+//                    new UrlResource(this.getClass().getClassLoader().getResource("test.jks")),
+                    new FileSystemResource(this.getClass().getClassLoader().getResource("keystore/test.jks").getPath()),
+                    "test1234".toCharArray()
                 )
-                .getKeyPair("auth", "passone".toCharArray());
+                .getKeyPair("asdf", "asdf1234".toCharArray());
 
         converter.setKeyPair(keyPair);
+        System.out.println("keyPair :: " + keyPair);
         return converter;
     }
 
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices(DataSource dataSource){
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        defaultTokenServices.setSupportRefreshToken(true);
-        return defaultTokenServices;
-    }
+//    @Bean
+//    @Primary
+//    public DefaultTokenServices tokenServices(DataSource dataSource){
+//        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+//        defaultTokenServices.setTokenStore(tokenStore());
+//        defaultTokenServices.setSupportRefreshToken(true);
+//        return defaultTokenServices;
+//    }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 //        super.configure(endpoints);
-        endpoints.tokenStore(tokenStore())
-                .authenticationManager(authenticationManager)
-                .accessTokenConverter(accessTokenConverter());
+//        endpoints.tokenStore(tokenStore())
+//                .authenticationManager(authenticationManager)
+//                .accessTokenConverter(accessTokenConverter());
+        endpoints.accessTokenConverter(accessTokenConverter())
+                .authenticationManager(this.authenticationManager);
     }
 
     @Override
