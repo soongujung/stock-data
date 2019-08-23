6d80521811a4d789fccac5fc259bcde148de29df
SGJUNG <playmymind@daum.net>
2019-08-23 13:19:22 +0900

    slack 연동정보 추가

--- py-scheduler/connections/toc_slack_info.py
+++ py-scheduler/connections/toc_slack_info.py
@@ -1,6 +1,6 @@
 busan_toc_slack = {
     "token": "",
-    "name": "",
-    "incoming_webhook_url": "",
-    "icon_emoji": ""
+    "name": "busan_toc",
+    "incoming_webhook_url": "https://hooks.slack.com/services/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",
+    "icon_emoji": ":shipit:"
 }
