68953470bb05066be81b3dd5f52c00d79b381035
SGJUNG <playmymind@daum.net>
2019-08-23 09:00:29 +0900

    [cliff] CLI 초기 구성  : 커맨드 로직 추가 (진행중)

--- py-scheduler/shell.py
+++ py-scheduler/shell.py
@@ -16,7 +16,7 @@ class CLiMonitoringToc(App):
 
     def __init__(self, stdout=None):
         super(CLiMonitoringToc, self).__init__(
-            description='hello,...',
+            description='hello....',
             version='0.0.1',
             command_manager=CommandManager('tutorial.cli'),
             deferred_help=True,
@@ -51,5 +51,31 @@ class CLiMonitoringToc(App):
     def initialize_app(self, argv):
         self.LOG.debug('initializing_app')
         self.command_manager.add_all_command_group()
+        self.command_manager.add_command_group('cli.common.module:ListCommand')
+        self.command_manager.add_command_group('cli.toc.chk_process.cmd1:ListCommand')
+
+        config_path = self.options.config
+        if os.path.exists(config_path):
+            self.config = configparser.ConfigParser()
+            self.config.read(config_path)
+
+    def prepare_to_run_command(self, cmd):
+        self.LOG.debug('prepare_to_run_command %s', cmd.__class__.__name__)
+
+    def clean_up(self, cmd, result, err):
+        self.LOG.debug('clean_up %s', cmd.__class__.__name__)
+        if err:
+            self.LOG.debug('got an error %s', err)
 
 
+def main(argv=sys.argv[1:]):
+    ret = CLiMonitoringToc.instance().run(argv)
+    if isinstance(ret, dict) or isinstance(ret, list):
+        print(ret)
+        return 0
+
+    return ret
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
