9acd5a9f81941a0bf99ff11f4f175d9505b63bab
SGJUNG <playmymind@daum.net>
2019-09-06 00:18:10 +0900

    [code]  : api 크롤링 로직 추가

--- py-crawling/crawler_kospi_daily.py
+++ py-crawling/crawler_kospi_daily.py
@@ -4,7 +4,8 @@ import psycopg2
 import pandas.io.sql as pandas_sql
 from pandas import DataFrame
 from sqlalchemy import create_engine
-
+import urllib3
+import json
 
 dict_account = {
     'host': 'localhost',
@@ -34,22 +35,26 @@ if __name__ == '__main__':
     conn = psycopg2.connect(conn_str)
     cursor = conn.cursor()
 
-    # cursor.execute("select * from stock_kospi_month")
-    # result = cursor.fetchall()
-    # print(result)
+    # --  api 크롤링
+    url = "http://ecos.bok.or.kr/api/StatisticSearch/[api키]/json/kr/1/10/028Y015/MM/201901/201906/1070000/"
+    http = urllib3.PoolManager()
+    ret = http.request("GET", url, headers={'Content-Type': 'application/json'})
+    print('=======================\n')
+    print('     HTTP RESULT         ')
+    print('=======================\n')
+    print('type of ret :: ', type(ret.data.decode('utf-8')))
+    print(ret.data.decode('utf-8'))
 
-    # INSERT INTO
-    # public.stock_kospi_month(
-    #     stat_name, item_code1, stat_code, item_code2, item_code3, item_name1, item_name2, item_name3, data_value,
-    #     "time")
-    # VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    str_response = ret.data.decode('utf-8')
+    dict_data = json.loads(str_response)
 
+    # -- database
     df_kospi_month = pandas_sql.read_sql_query("select * from stock_kospi_month", conn)
     print(df_kospi_month)
 
     arr_columns = [
-        'stat_name' ,'stat_code' ,'item_code1','item_code2','item_code3',
-        'item_name1','item_name2','item_name3','data_value','time'
+        'stat_name',  'stat_code',  'item_code1', 'item_code2', 'item_code3',
+        'item_name1', 'item_name2', 'item_name3', 'data_value', 'time'
     ]
 
     sample_data = {
@@ -69,7 +74,7 @@ if __name__ == '__main__':
     df_kospi_insert.to_sql(name='stock_kospi_month',
                            con=alchemy_conn,
                            index=False,
-                           if_exists='append', #{'fail', 'replace', 'append'}, default : fail
+                           if_exists='replace', #{'fail', 'replace', 'append'}, default : fail
                            schema='public')
 
     df_kospi_month = pandas_sql.read_sql_query("select * from stock_kospi_month", alchemy_conn)
