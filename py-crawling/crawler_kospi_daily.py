c64db49c945c6d01c35172376e9081eea58f79f7
SGJUNG <playmymind@daum.net>
2019-09-07 00:09:00 +0900

    [code]  : database insert 구문 추가 (using pandas)

--- py-crawling/crawler_kospi_daily.py
+++ py-crawling/crawler_kospi_daily.py
@@ -31,46 +31,32 @@ alchemy_conn = create_engine(
         )
 )
 
+arr_columns = [
+        'STAT_NAME',  'STAT_CODE',  'ITEM_CODE1', 'ITEM_CODE2', 'ITEM_CODE3',
+        'ITEM_NAME1', 'ITEM_NAME2', 'ITEM_NAME3', 'DATA_VALUE', 'TIME'
+    ]
+
+
 if __name__ == '__main__':
     conn = psycopg2.connect(conn_str)
     cursor = conn.cursor()
 
     # --  api 크롤링
-    url = "http://ecos.bok.or.kr/api/StatisticSearch/[api키]/json/kr/1/10/028Y015/MM/201901/201906/1070000/"
+    url = "http://ecos.bok.or.kr/api/StatisticSearch/RV1NS82MWHJGX93N28C2/json/kr/1/10/028Y015/MM/201901/201906/1070000/"
     http = urllib3.PoolManager()
     ret = http.request("GET", url, headers={'Content-Type': 'application/json'})
     print('=======================\n')
     print('     HTTP RESULT         ')
     print('=======================\n')
-    print('type of ret :: ', type(ret.data.decode('utf-8')))
-    print(ret.data.decode('utf-8'))
 
     str_response = ret.data.decode('utf-8')
     dict_data = json.loads(str_response)
+    arr_data = dict_data['StatisticSearch']['row']
 
-    # -- database
-    df_kospi_month = pandas_sql.read_sql_query("select * from stock_kospi_month", conn)
-    print(df_kospi_month)
+    df_kospi_insert = DataFrame(arr_data, columns=arr_columns)
+    print('df_kospi_insert : ' + df_kospi_insert)
 
-    arr_columns = [
-        'stat_name',  'stat_code',  'item_code1', 'item_code2', 'item_code3',
-        'item_name1', 'item_name2', 'item_name3', 'data_value', 'time'
-    ]
-
-    sample_data = {
-        'stat_name': [3,4,5,6],
-        'stat_code': [3,4,5,6],
-        'item_code1': [3,4,5,6],
-        'item_code2': [3,4,5,6],
-        'item_code3': [3,4,5,6],
-        'item_name1': [3,4,5,6],
-        'item_name2': [3,4,5,6],
-        'item_name3': [3,4,5,6],
-        'data_value': [3,4,5,6],
-        'time': [3,4,5,6],
-    }
-
-    df_kospi_insert = DataFrame(sample_data, columns=arr_columns)
+    # -- database insert/select
     df_kospi_insert.to_sql(name='stock_kospi_month',
                            con=alchemy_conn,
                            index=False,
