1fd809032dbc06f7e4946f66ada476c558c140f0
SGJUNG <playmymind@daum.net>
2019-09-11 13:16:02 +0900

    [code]  : 디폴트 기간 검색조건(1년)에 대한 코드 추가

--- stock-data-api/src/main/java/com/trending/web/closingprice/ClosingPriceController.java
+++ stock-data-api/src/main/java/com/trending/web/closingprice/ClosingPriceController.java
@@ -1,11 +1,21 @@
 package com.trending.web.closingprice;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.types.date.localdate.FormatterTypes;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,18 +24,25 @@ import java.util.Map;
 public class ClosingPriceController {
 
     @Autowired
-    private SqlSession sqlSession;
-
-    @Autowired
     private ClosingPriceService closingPriceService;
 
     @GetMapping("/trending/web/closing_price/index")
-    public String getIndex(){
+    public String getIndex(HttpServletRequest request, Model model) throws JsonProcessingException {
 //        return "/trending/web/closing_price/index";
         Map<String,Object> params = new HashMap<>();
-        params.put("startDate", "2018-01-01");
-        params.put("endDate", "2018-01-30");
+
+        LocalDate endDate = LocalDate.now();
+        LocalDate startDate = endDate.minus(1, ChronoUnit.YEARS);
+
+        String strStartDate = startDate.format(FormatterTypes.YYYYMMDD.ofPattern());
+        String strEndDate = endDate.format(FormatterTypes.YYYYMMDD.ofPattern());
+
+        params.put("startDate", strStartDate);
+        params.put("endDate", strEndDate);
+
         List<Map<String,Object>> kospiResult = closingPriceService.getKospiResult(params);
+        model.addAttribute("kospiResult", new ObjectMapper().writeValueAsString(kospiResult));
+
         return "/trending/web/closing_price/index";
     }
 }
