5676c1d4bf6d9856359739f6e454b628c3bf5c48
SGJUNG <playmymind@daum.net>
2019-09-13 00:11:10 +0900

    [code]  : 환율 DDL SQL 추가

---
+++ py-crawling/sql/ddl/economy_exchange_rate_day.sql
@@ -0,0 +1,26 @@
+create table economy_exchange_rate_day
+(
+  stat_name  varchar(40),
+  stat_code  varchar(20),
+  item_code1 varchar(20),
+  item_name1 varchar(20),
+  item_code2 varchar(20),
+  item_name2 varchar(20),
+  item_code3 varchar(20),
+  item_name3 varchar(20),
+  data_value varchar(20),
+  time       varchar(14) not null
+    constraint economy_exchange_rate_day_pk
+      primary key
+);
+
+alter table economy_exchange_rate_day
+  owner to postgres;
+
+create index economy_exchange_rate_day_time_index
+  on economy_exchange_rate_day (time);
+
+commit;
+
+
+
