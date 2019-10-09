92f7d147b0624b68dae45dec4006d0623d8a1365
SGJUNG <playmymind@daum.net>
2019-10-09 14:11:50 +0900

    [code]  : 자료형 명시적 지정 코드 추가

--- py-crawling/crawler_kospi_day.py
+++ py-crawling/crawler_kospi_day.py
@@ -6,6 +6,7 @@ from pandas import DataFrame
 from sqlalchemy import create_engine
 import urllib3
 import json
+from credentials import api_keys
 
 dict_account = {
     'host': 'localhost',
@@ -36,13 +37,28 @@ arr_columns = [
         'ITEM_NAME1', 'ITEM_NAME2', 'ITEM_NAME3', 'DATA_VALUE', 'TIME'
     ]
 
+dict_columns_type = {
+    'STAT_NAME': 'str',
+    'STAT_CODE': 'str',
+    'ITEM_CODE1': 'str',
+    'ITEM_CODE2': 'str',
+    'ITEM_CODE3': 'str',
+    'ITEM_NAME1': 'str',
+    'ITEM_NAME2': 'str',
+    'ITEM_NAME3': 'str',
+    'DATA_VALUE': 'float',
+    'TIME': 'str'
+}
+api_key = api_keys.kor_bank_api['api_key']
+
 
 if __name__ == '__main__':
     conn = psycopg2.connect(conn_str)
     cursor = conn.cursor()
 
     # --  api 크롤링
-    url = "http://ecos.bok.or.kr/api/StatisticSearch/[api키]/json/kr/1/365/064Y001/DD/20180101/20181231/0001000"
+    url = "http://ecos.bok.or.kr/api/StatisticSearch/{}/json/kr/1/5000/064Y001/DD/20140101/20191231/0001000".format(api_key)
+
     http = urllib3.PoolManager()
     ret = http.request("GET", url, headers={'Content-Type': 'application/json'})
 
@@ -51,8 +67,11 @@ if __name__ == '__main__':
     dict_data = json.loads(str_response)
     arr_data = dict_data['StatisticSearch']['row']
 
-    df_kospi_insert = DataFrame(arr_data, columns=arr_columns)
-    print('df_kospi_insert : ' + df_kospi_insert)
+    data_for_insert = [[ dict_data[column_nm] for column_nm in arr_columns] for dict_data in arr_data]
+
+    df_kospi_insert = DataFrame(data_for_insert, columns=arr_columns)
+    df_kospi_insert = df_kospi_insert.astype(dtype=dict_columns_type)
+    # print('df_kospi_insert : ' + df_kospi_insert)
 
     # -- database insert/select
     df_kospi_insert.to_sql(name='stock_kospi_day',
@@ -61,11 +80,7 @@ if __name__ == '__main__':
                            if_exists='replace', #{'fail', 'replace', 'append'}, default : fail
                            schema='public')
 
-    df_kospi_month = pandas_sql.read_sql_query("select * from stock_kospi_day", alchemy_conn)
+    df_kospi_select = pandas_sql.read_sql_query("select * from stock_kospi_day", alchemy_conn)
     print("database result ::: stock_kospi_month")
-    print(df_kospi_month)
-
-
-
-
+    print(df_kospi_select)
 
