6db0a38882bb50e701b5f9b3e66464363941d8ba
sgjung <playmymind@daum.net>
2020-01-24 18:55:31 +0900

    테스트 코드 추가

--- stock-api/src/test/java/com/stock/data/web/HelloControllerTest.java
+++ stock-api/src/test/java/com/stock/data/web/HelloControllerTest.java
@@ -2,13 +2,28 @@ package com.stock.data.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.junit.Assert.*;
 
-//@RunWith(SpringRunner) -- spring boot starter junit 의존성 추가 필요
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class HelloControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    public void hello() {
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
     }
 }
