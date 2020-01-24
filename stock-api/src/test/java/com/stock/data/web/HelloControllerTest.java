671d27ca05d0d751c26cf9cec056ee783b373667
sgjung <playmymind@daum.net>
2020-01-24 18:31:50 +0900

    ignore 파일 추가

---
+++ stock-api/src/test/java/com/stock/data/web/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.stock.data.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+//@RunWith(SpringRunner) -- spring boot starter junit 의존성 추가 필요
+public class HelloControllerTest {
+
+    @Test
+    public void hello() {
+    }
+}
