ef3422e75838360a1a3875069bee38bd11d21aba
kyle.sgjung <kyle.sgjung@kakaocorp.com>
2020-01-25 20:19:45 +0900

    jpa 기본 코드 커밋

---
+++ stock-api/src/test/java/com/stock/data/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.stock.data.domain.posts;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+	@Autowired
+	PostsRepository postsRepository;
+
+	@After
+	public void cleanup(){
+		postsRepository.deleteAll();
+	}
+
+	@Test
+	public void 게시글_저장후_불러오기(){
+		String title = "테스트 제목 1";
+		String content = "테스트 본문";
+		String author = "helloSydney@gmail.com";
+
+		postsRepository.save(
+			new Posts(title, content, author)
+		);
+
+		List<Posts> postsList = postsRepository.findAll();
+
+		Posts posts = postsList.get(0);
+//		assertThat(posts.getTitle()).isEqualTo(title);
+//		assertThat(posts.getContent()).isEqualTo(content);
+	}
+}
