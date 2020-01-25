a579b94c428b9f6f54b33550a73f08069e147d36
kyle.sgjung <kyle.sgjung@kakaocorp.com>
2020-01-25 20:44:25 +0900

    jpa 기본 코드 (application.properties)

--- stock-api/src/test/java/com/stock/data/domain/posts/PostsRepositoryTest.java
+++ stock-api/src/test/java/com/stock/data/domain/posts/PostsRepositoryTest.java
@@ -28,6 +28,16 @@ public class PostsRepositoryTest {
 		String content = "테스트 본문";
 		String author = "helloSydney@gmail.com";
 
+		// 롬복이 작동할 때는 아래 코드로...
+//		postsRepository.save(
+//			Posts.builder()
+//				.content(content)
+//				.title(title)
+//				.author(author)
+//				.build()
+//		);
+
+		// 롬복이 IDE에서 정상동작하지 않는다면...
 		postsRepository.save(
 			new Posts(title, content, author)
 		);
