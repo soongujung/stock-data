b457c4e1aa967a967c6bb8e686e66b90663758c6
kyle.sgjung <kyle.sgjung@kakaocorp.com>
2020-01-27 14:09:12 +0900

    Entity Auditing 기능(Time) with JPA

--- stock-api/src/test/java/com/stock/data/domain/posts/PostsRepositoryTest.java
+++ stock-api/src/test/java/com/stock/data/domain/posts/PostsRepositoryTest.java
@@ -2,6 +2,7 @@ package com.stock.data.domain.posts;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.After;
 import org.junit.Test;
@@ -41,5 +42,30 @@ public class PostsRepositoryTest {
 		Posts posts = postsList.get(0);
 		assertThat(posts.getTitle()).isEqualTo(title);
 		assertThat(posts.getContent()).isEqualTo(content);
+	}
+
+	@Test
+	public void BaseTimeEntity_등록(){
+		LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0,0, 0);
+		postsRepository.save(
+			Posts.builder()
+				.title("title")
+				.content("content")
+				.author("author")
+				.build()
+		);
+
+		List<Posts> postsList = postsRepository.findAll();
+
+		Posts post = postsList.get(0);
+
+		System.out.println("====== createdDate :: "
+			+ post.getCreatedDate()
+			+ ", modifiedDate :: "
+			+ post.getModifiedDate()
+		);
+
+		assertThat(post.getCreatedDate()).isAfter(now);
+		assertThat(post.getModifiedDate()).isAfter(now);
 	}
 }
