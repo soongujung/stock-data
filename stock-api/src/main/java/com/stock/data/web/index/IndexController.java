a4dd81c8556ffebf7ede4fd047209fe0c3f5eb93
kyle.sgjung <kyle.sgjung@kakaocorp.com>
2020-02-01 19:56:39 +0900

    시큐리티 + 어노테이션  : 로그인 여부 확인 로직 어노테이션 기반으로 수정

--- stock-api/src/main/java/com/stock/data/web/index/IndexController.java
+++ stock-api/src/main/java/com/stock/data/web/index/IndexController.java
@@ -1,5 +1,6 @@
 package com.stock.data.web.index;
 
+import com.stock.data.config.auth.LoginUser;
 import com.stock.data.config.auth.dto.SessionUser;
 import com.stock.data.web.posts.PostsService;
 import javax.servlet.http.HttpSession;
@@ -16,8 +17,8 @@ public class IndexController {
 	private final HttpSession httpSession;
 
 	@GetMapping("/")
-	public String index(Model model){
-		SessionUser user = (SessionUser) httpSession.getAttribute("user");
+	public String index(Model model, @LoginUser SessionUser user){
+//		SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
 		if(user != null){
 			model.addAttribute("userName", user.getName());
